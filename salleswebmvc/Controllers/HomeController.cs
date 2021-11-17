3478b9d8709a3d14e7ed2db9237ab106b061ecbe
rbtgyn <rob3r7@oulook.com>
2021-11-17 20:44:39 -0300

    Firt controller and Razor pages teste

--- salleswebmvc/Controllers/HomeController.cs
+++ salleswebmvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace salleswebmvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC app fron c# Course";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
