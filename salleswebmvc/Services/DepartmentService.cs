489e0cbd28db9dd243f170998503e39cd3fe3b5c
rbtgyn <rob3r7@oulook.com>
2021-12-02 19:53:00 -0300

    SellerFormViewModel and Department select componente

---
+++ salleswebmvc/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using salleswebmvc.Data;
+using salleswebmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace salleswebmvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly salleswebmvcContext _context;
+
+        public DepartmentService(salleswebmvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
