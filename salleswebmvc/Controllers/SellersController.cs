c26b7ba5387f30bad3aba4366be70ee784f8ab09
rbtgyn <rob3r7@oulook.com>
2021-12-07 18:00:51 -0300

    Exception handling for delete (referential integrity)

--- salleswebmvc/Controllers/SellersController.cs
+++ salleswebmvc/Controllers/SellersController.cs
@@ -68,8 +68,15 @@ namespace salleswebmvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
 
         }
 
--- salleswebmvc/Services/SellerService.cs
+++ salleswebmvc/Services/SellerService.cs
@@ -28,9 +28,16 @@ namespace salleswebmvc.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj =  await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
         }
         public async Task InsertAsync(Seller obj)
         {
